75c682e7e85b2c29c6803b8b7a43997508751a81
Kreidina <kav99mail@gmail.com>
2023-11-13 20:16:20 +0200

    start header and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <>
+      <p>logo</p>
+      <nav>
+        <ul>
+          <li>Головна</li>
+          <li>Посуд</li>
+          <li>Доставка та оплата</li>
+          <li>Відгуки</li>
+        </ul>
+      </nav>
+      <div>
+        <p>tell</p>
+        <p>icon insta</p>
+        <p>icon face</p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
